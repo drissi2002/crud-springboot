1798ea222ddbff30c8672196f2d6e15846a2d41e
drissi houcem eddine <84160502+drissi2002@users.noreply.github.com>
2022-04-05 23:56:09 +0100

    Add files via upload

---
+++ springCRUD/employeemanagerBack/src/main/java/tech/getarrays/employeemanager/Exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package tech.getarrays.employeemanager.Exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
